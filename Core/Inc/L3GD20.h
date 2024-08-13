9d527879f04b6c08f650c4baccf2fd3af99cb234
hmquyet <quyet.hoangminh.sistrain@gmail.com>
2024-08-13 22:05:18 +0700

    update

--- Core/Inc/L3GD20.h
+++ Core/Inc/L3GD20.h
@@ -1,45 +1,42 @@
+#ifndef L3GD20_H
+#define L3GD20_H
 
+#include <stdint.h>
 
-#ifndef __L3GD20_H
-#define __L3GD20_H
+// L3GD20 Register Definitions
+#define L3GD20_REG_WHO_AM_I    0x0F
+#define L3GD20_REG_CTRL1       0x20
+#define L3GD20_REG_CTRL2       0x21
+#define L3GD20_REG_CTRL3       0x22
+#define L3GD20_REG_CTRL4       0x23
+#define L3GD20_REG_CTRL5       0x24
 
-#ifdef __cplusplus
- extern "C" {
-#endif
-	 
-	
-	 
-	 
-	 void L3GD20_Init(void);
-	 
-	 void L3GD20_loop(void);
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 #ifdef __cplusplus
-}
-#endif
+#define L3GD20_REG_OUT_X_L     0x28
+#define L3GD20_REG_OUT_X_H     0x29
+#define L3GD20_REG_OUT_Y_L     0x2A
+#define L3GD20_REG_OUT_Y_H     0x2B
+#define L3GD20_REG_OUT_Z_L     0x2C
+#define L3GD20_REG_OUT_Z_H     0x2D
+
+// Sensitivity
+#define L3GD20_SENSITIVITY_250DPS   0.00875f
+#define L3GD20_SENSITIVITY_500DPS   0.0175f
+#define L3GD20_SENSITIVITY_2000DPS  0.07f
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} L3GD20_RawData_t;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} L3GD20_AngleRate_t;
+
+void L3GD20_Init(void);
+void L3GD20_ReadRawData(L3GD20_RawData_t *rawData);
+void L3GD20_CalculateAngleRate(const L3GD20_RawData_t *rawData, L3GD20_AngleRate_t *angleRate);
 
 #endif
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -30,77 +30,6 @@
 /* USER CODE BEGIN PTD */
 
 
-#define AVERAGE_WINDOW_SIZE                  ((uint32_t) 10u)
-#define CALIBRATION_BUFFER_LENGTH            ((uint32_t) 2000u)
-#define L3GD20_SENSITIVITY    ((float)0.07)
-
-
-typedef enum
-{
-	L3GD20_DATA_NOT_READY,
-	L3GD20_DATA_READY,
-}L3GD20_DataReadyFlagType;
-
-static L3GD20_DataReadyFlagType dataReadyFlag=L3GD20_DATA_READY;
-
-typedef enum
-{
-	L3GD20_collect_calibration_samples,
-	L3GD20_process_calibration_samples,
-	L3GD20_calibrated,
-}L3GD20_caliStateType;
-
-static L3GD20_caliStateType currentcalistate=L3GD20_collect_calibration_samples;
-
-
-
-typedef enum
-{
-	L3GD20_fisrt,
-	L3GD20_second,
-	L3GD20_finaly
-}L3GD20_StateType;
-static L3GD20_StateType currentState=L3GD20_fisrt;
-	
-
-static float angleRate_x=0;
-static float angleRate_y=0;
-static float angleRate_z=0;
-
-static int32_t offset_x=0;
-static int32_t offset_y=0;
-static int32_t offset_z=0;
-
-static float Noise_X = 0;
-static float Noise_Y = 0;
-static float Noise_Z = 0;
-
-static float Angle_X = 0;
-static float Angle_Y = 0;
-static float Angle_Z = 0;
-
-static float LastAngleRate_X = 0;
-static float LastAngleRate_Y = 0;
-static float LastAngleRate_Z = 0;
-
-static int32_t TempNoise_X = 0;
-static int32_t TempNoise_Y = 0;
-static int32_t TempNoise_Z = 0;
-
-
-
-
-
-
-volatile static uint32_t caliCounter = 0;
-
-
-static int16_t calibrationBuffer_X[CALIBRATION_BUFFER_LENGTH];
-static int16_t calibrationBuffer_Y[CALIBRATION_BUFFER_LENGTH];
-static int16_t calibrationBuffer_Z[CALIBRATION_BUFFER_LENGTH];
-
-static uint8_t spiTxBuf[2];
-static uint8_t spiRxBuf[7];
 
 /* USER CODE END PTD */
 
@@ -116,274 +45,34 @@ static uint8_t spiRxBuf[7];
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-void L3GD20_Init(void)
-{
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-	HAL_Delay(20);
-	
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-	HAL_Delay(20);
-	spiTxBuf[0]=0x20;
-	spiTxBuf[1]=0xff;
-	HAL_SPI_Transmit(&hspi1,spiTxBuf,2,50);
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-	HAL_Delay(20);
-	
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-	HAL_Delay(20);
-	spiTxBuf[0]=0x21;
-	spiTxBuf[1]=0x00;
-	HAL_SPI_Transmit(&hspi1,spiTxBuf,2,50);
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-	HAL_Delay(20);
-	
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-	HAL_Delay(20);
-	spiTxBuf[0]=0x22;
-	spiTxBuf[1]=0x00;
-	HAL_SPI_Transmit(&hspi1,spiTxBuf,2,50);
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-	HAL_Delay(20);
-	
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-	HAL_Delay(20);
-	spiTxBuf[0]=0x23;
-	spiTxBuf[1]=0x20;
-	HAL_SPI_Transmit(&hspi1,spiTxBuf,2,50);
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-	HAL_Delay(20);
-	
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-	HAL_Delay(20);
-	spiTxBuf[0]=0x24;
-	spiTxBuf[1]=0x10;
-	HAL_SPI_Transmit(&hspi1,spiTxBuf,2,50);
-	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-	HAL_Delay(20);
-	
-}
-
-void L3GD20_loop(void)
-{
-		volatile int16_t Raw_x=0;
-		volatile int16_t Raw_y=0;
-		volatile int16_t Raw_z=0;
-
-		float difftime=0;
-		
-		int16_t averageWindow_X[AVERAGE_WINDOW_SIZE] = {0};
-		int16_t averageWindow_Y[AVERAGE_WINDOW_SIZE] = {0};
-		int16_t averageWindow_Z[AVERAGE_WINDOW_SIZE] = {0};
-		
-		uint32_t windowPosition = 0;
-		int32_t tempSum_X = 0;
-		int32_t tempSum_Y = 0;
-		int32_t tempSum_Z = 0;
-		
-		switch(currentState)
-		{
-			//---------------------------------------------------------------------------
-			//data 
-			case(L3GD20_fisrt):
-				if(dataReadyFlag==L3GD20_DATA_READY)
-				{
-					HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-						spiTxBuf[0]=0x28|0x80;
-						HAL_SPI_Transmit(&hspi1,spiTxBuf,1,50);
-						HAL_SPI_Receive(&hspi1,&spiRxBuf[1],1,50);
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-							
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-						spiTxBuf[0]=0x29|0x80;
-						HAL_SPI_Transmit(&hspi1,spiTxBuf,1,50);
-						HAL_SPI_Receive(&hspi1,&spiRxBuf[2],1,50);
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-							
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-						spiTxBuf[0]=0x2a|0x80;
-						HAL_SPI_Transmit(&hspi1,spiTxBuf,1,50);
-						HAL_SPI_Receive(&hspi1,&spiRxBuf[3],1,50);
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-							
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-						spiTxBuf[0]=0x2b|0x80;
-						HAL_SPI_Transmit(&hspi1,spiTxBuf,1,50);
-						HAL_SPI_Receive(&hspi1,&spiRxBuf[4],1,50);
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-						
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-						spiTxBuf[0]=0x2c|0x80;
-						HAL_SPI_Transmit(&hspi1,spiTxBuf,1,50);
-						HAL_SPI_Receive(&hspi1,&spiRxBuf[5],1,50);
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-						
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_RESET);
-						spiTxBuf[0]=0x2d|0x80;
-						HAL_SPI_Transmit(&hspi1,spiTxBuf,1,50);
-						HAL_SPI_Receive(&hspi1,&spiRxBuf[6],1,50);
-						HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,GPIO_PIN_SET);
-						
-						currentState=L3GD20_second;
-						dataReadyFlag=L3GD20_DATA_NOT_READY;
-				}
-				else
-				{
-				}
-						
-				break;
-						//-----------------------------------------------------------------------------------
-						//varibla
-			case(L3GD20_second):
-				Raw_x=(spiRxBuf[2]<<8)|spiRxBuf[1];
-				Raw_y=(spiRxBuf[4]<<8)|spiRxBuf[3];
-				Raw_z=(spiRxBuf[6]<<8)|spiRxBuf[5];
-			
-			if(currentcalistate==L3GD20_calibrated)
-			{
-				angleRate_x=(float) (Raw_x - (offset_x))*L3GD20_SENSITIVITY;
-				angleRate_y=(float) (Raw_y - (offset_y))*L3GD20_SENSITIVITY;
-				angleRate_z=(float) (Raw_z - (offset_z))*L3GD20_SENSITIVITY;
-				difftime=0.003f;
-
-				if((angleRate_x>Noise_X)||(angleRate_x<-Noise_X))
-				{
-					Angle_X+=((angleRate_x+LastAngleRate_X)*difftime)/(2.0f);
-					LastAngleRate_X=angleRate_x;
-				}
-				else
-				{
-				}
-				if((angleRate_y>Noise_Y)||(angleRate_y<-Noise_Y))
-				{
-					Angle_Y+=((angleRate_y+LastAngleRate_Y)*difftime)/(2.0f);
-					LastAngleRate_Y=angleRate_y;
-				}
-				else
-				{
-				}
-				if((angleRate_z>Noise_Z)||(angleRate_z<-Noise_Z))
-				{
-					Angle_Z+=((angleRate_z+LastAngleRate_Z)*difftime)/(2.0f);
-					LastAngleRate_Z=angleRate_z;
-				}
-				else
-				{
-				}
-			}
-			else
-			{
-				switch(currentcalistate)
-				{
-					//---------------------------------------------------------------------------------------------------------
-					case(L3GD20_collect_calibration_samples):
-						calibrationBuffer_X[caliCounter]=Raw_x;
-						calibrationBuffer_Y[caliCounter]=Raw_y;
-						calibrationBuffer_Z[caliCounter]=Raw_z;
-						caliCounter++;
-					
-						if(caliCounter>=CALIBRATION_BUFFER_LENGTH)
-						{
-							caliCounter=0;
-							
-							currentcalistate=L3GD20_process_calibration_samples;
-						}
-						else
-						{
-						}
-						break;
-					//----------------------------------------------------------------------------------------------------------	
-					case(L3GD20_process_calibration_samples):
-						for(uint32_t idx=0; idx<CALIBRATION_BUFFER_LENGTH;idx++)
-							{
-								tempSum_X=tempSum_X-averageWindow_X[windowPosition]+calibrationBuffer_X[idx];
-								tempSum_Y=tempSum_Y-averageWindow_Y[windowPosition]+calibrationBuffer_Y[idx];
-								tempSum_Z=tempSum_Z-averageWindow_Z[windowPosition]+calibrationBuffer_Z[idx];
-								
-								averageWindow_X[windowPosition]=calibrationBuffer_X[idx];
-								averageWindow_Y[windowPosition]=calibrationBuffer_Y[idx];
-								averageWindow_Z[windowPosition]=calibrationBuffer_Z[idx];
-								
-								offset_x=tempSum_X/(int32_t)AVERAGE_WINDOW_SIZE;
-								offset_y=tempSum_Y/(int32_t)AVERAGE_WINDOW_SIZE;
-								offset_z=tempSum_Z/(int32_t)AVERAGE_WINDOW_SIZE;
-								
-								windowPosition++;
-								
-								if(windowPosition>=AVERAGE_WINDOW_SIZE)
-								{
-									windowPosition=0;
-								}
-								else
-								{
-								}
-								
-							}
-							for(uint32_t idx=0;idx<CALIBRATION_BUFFER_LENGTH;idx++)
-							{
-								if(((int32_t)calibrationBuffer_X[idx]-offset_x)>TempNoise_X)
-								{
-									TempNoise_X=(int32_t)calibrationBuffer_X[idx]-offset_x;
-								}
-								else if(((int32_t)calibrationBuffer_X[idx]-offset_x)<-TempNoise_X)
-								{
-									TempNoise_X=-((int32_t)calibrationBuffer_X[idx]-offset_x);
-								}
-								
-								if(((int32_t)calibrationBuffer_Y[idx]-offset_y)>TempNoise_Y)
-								{
-									TempNoise_Y=(int32_t)calibrationBuffer_Y[idx]-offset_y;
-								}
-								else if(((int32_t)calibrationBuffer_Y[idx]-offset_y)<-TempNoise_Y)
-								{
-									TempNoise_Y=-((int32_t)calibrationBuffer_Y[idx]-offset_y);
-								}
-								
-								if(((int32_t)calibrationBuffer_Z[idx]-offset_z)>TempNoise_Z)
-								{
-									TempNoise_Z=(int32_t)calibrationBuffer_Z[idx]-offset_z;
-								}
-								else if(((int32_t)calibrationBuffer_Z[idx]-offset_z)<-TempNoise_Z)
-								{
-									TempNoise_Z=-((int32_t)calibrationBuffer_Z[idx]-offset_z);
-								}
-							}
-							
-							Noise_X=(float)TempNoise_X*L3GD20_SENSITIVITY;
-							Noise_Y=(float)TempNoise_Y*L3GD20_SENSITIVITY;
-							Noise_Z=(float)TempNoise_Z*L3GD20_SENSITIVITY;
-							
-							currentcalistate=L3GD20_calibrated;
-							break;
-							
-					case(L3GD20_calibrated):
-						break;
-					
-					default:
-						break;
-				}
-						
-			}
-			currentState=L3GD20_fisrt;
-			dataReadyFlag=L3GD20_DATA_READY;
-			break;
-			
-					default:
-						break;
-			
-		}
-		//printf(Angle_X);
-}
-
+void SPI_Transmit(uint8_t *data, size_t length);
+void SPI_TransmitReceive(uint8_t *txData, uint8_t *rxData, size_t length);
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+    L3GD20_RawData_t rawData;
+    L3GD20_AngleRate_t angleRate;
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+void SPI_Transmit(uint8_t *data, size_t length) {
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET); 
+	
+    HAL_SPI_Transmit(&hspi1, data, length, HAL_MAX_DELAY);
+
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);  
+}
+
+void SPI_TransmitReceive(uint8_t *txData, uint8_t *rxData, size_t length) {
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET); 
+	
+    HAL_SPI_TransmitReceive(&hspi1, txData, rxData, length, HAL_MAX_DELAY);
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);   
+}
 
 /* USER CODE END 0 */
 
@@ -417,7 +106,9 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-	L3GD20_Init();
+	    L3GD20_Init();                
+
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -427,8 +118,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-		L3GD20_loop();
-		HAL_Delay(1);
+		L3GD20_ReadRawData(&rawData);
+		L3GD20_CalculateAngleRate(&rawData, &angleRate);
+		//printf("Angle Rate: X=%f Y=%f Z=%f\n", angleRate.x, angleRate.y, angleRate.z);
+		HAL_Delay(10);  
   }
   /* USER CODE END 3 */
 }
